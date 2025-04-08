68a0d0638188657751e6a89d82177c326f2937ed
Miguel Angel Feria Padilla <migferiapad@gmail.com>
2025-04-08 11:20:20 -0600

    Modelo OpenIA

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 import os
+import openai
 from dotenv import load_dotenv
-import requests
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
 
-if not API_KEY:
-    raise ValueError("API_KEY not found in environment variables. Please set it in your .env file.")
+while True:
 
-url = "https://api.deepseek.com/v1/chat/completions"
+    prompt = input("\nIntroduce una pregunta:")
+    if prompt.lower() == "exit":
+        break
 
-headers = {
-    "Autorization" : f"Bearer {API_KEY}",
-    "Content-Type": "application/json"
-}
-data = {
-    "model" : "deepseek-chat",
-    "messages": [
-        {
-            "role": "user", "content": "Hola, ¿puedes explicarme cómo usar la API de DeepSeek?"
-        }
-    ]
-}
+    complection = openai.completions.create(engine= "gpt-3.5-turbo",
+                            prompt=prompt,
+                            max_tokens="1000")
 
-response = requests.post(url, headers=headers, json=data)
-if response.status_code == 200:
-    print("Response:", response.json())
-else:
-    print("Error:", response.status_code,response.text)
+    print(complection.choices[0].text)
