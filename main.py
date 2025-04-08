fe90b8248e650e075ae04998ada22e957775e86d
inkmiguel <migferiapad@gmail.com>
2025-04-08 13:11:31 -0600

    Codigo Actualizado

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# Cargar variables de entorno
 load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
 while True:
-
-    prompt = input("\nIntroduce una pregunta:")
+    prompt:str = input("\nIntroduce una pregunta: ")
     if prompt.lower() == "exit":
         break
 
-    complection = openai.completions.create(engine= "gpt-3.5-turbo",
-                            prompt=prompt,
-                            max_tokens="1000")
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=1000
+    )
 
-    print(complection.choices[0].text)
+    print(response.choices[0].message.content)
